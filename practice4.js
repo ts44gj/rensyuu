30b36f9425dd474c643298c0710875afcecb892d
ts44gj <ts44gj2020@gmail.com>
2020-10-27 11:34:24 +0900

    s

---
+++ practice4.js
@@ -0,0 +1,11 @@
+function bb() {
+   document.getElementById("idbb").style.backgroundColor = "black";
+      }
+function wm() {
+    document.getElementById("idwm").style.color = "white";}
+        
+function wb() {
+    document.getElementById("idwb").style.backgroundColor = "white";
+           }
+function bm() {
+    document.getElementById("idbm").style.color = "black";}
